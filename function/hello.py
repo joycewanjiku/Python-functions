d6e27835d433b00b69b3c56b2be94dce407c9228
student <joycemumbiiwanjiku@gmail.com>
2023-03-29 23:21:12 +0300

    pythonfunctions

---
+++ function/hello.py
@@ -0,0 +1,5 @@
+def hello(name):
+    print(f"Hello {name}")
+
+def welcom(student,school):
+    print(f"welcome to {school},{student}")
---
+++ function/math_1.py
@@ -0,0 +1,26 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtraction(d,c):
+    answer=d-c
+    return answer
+
+
+def division(j,m):
+    answer=j/m
+    return answer
+
+
+def multiply(k,n):
+    answer=k*n
+    return answer
+
+
+def modulus(l,p):
+    answer=l%p
+    return answer
+
+
+    
+
